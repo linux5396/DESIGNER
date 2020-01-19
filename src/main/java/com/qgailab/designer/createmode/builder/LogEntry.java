b1f5293ac34fe7e6e9659514afe08635f8e19e1a
linux5396 <929159338@qq.com>
2020-01-19 20:09:26 +0800

    add builder

---
+++ src/main/java/com/qgailab/designer/createmode/builder/LogEntry.java
@@ -0,0 +1,9 @@
+package com.qgailab.designer.createmode.builder;
+
+/**
+ * @author linxu
+ * @date 2020/1/19
+ * <tip>take care of yourself.everything is no in vain.</tip>
+ */
+public class LogEntry {
+}
